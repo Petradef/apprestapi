e08957c3694d3eec5cc712aae4107c6fa84f3bb1
petradef <petra.defretes88@gmail.com>
2020-12-08 11:04:21 +0700

    berhasil menampilkan semua data karyawan

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require("./koneksi")
 exports.index = function (req, res) {
     response.ok("Aplikasi REST API Ku Berjalan!", res)
 }
+
+//menampilkan semua data karyawan
+exports.tampilsemuakaryawan = function (req, res) {
+    connection.query("SELECT * from karyawan", function (error, rows, fields) {
+        if (error) {
+            connection.log(error)
+        } else {
+            response.ok(rows, res)
+        }
+    })
+}
--- routes.js
+++ routes.js
@@ -4,5 +4,7 @@ module.exports = function (app) {
     var jsonku = require("./controller")
 
     app.route("/").get(jsonku.index)
+
+    app.route("/tampil").get(jsonku.tampilsemuakaryawan)
 }
 
